2498dd35b2dd900526602c333548cc17b6dda224
josh2397 <josh23797@gmail.com>
2019-05-20 17:19:40 +1000

    Added Trend and Home Components on Laptop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import {RouterModule, Routes} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { TrendComponent } from './trend/trend.component';
+import { HomeComponent } from './home/home.component';
 
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrendComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
